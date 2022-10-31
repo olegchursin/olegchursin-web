a879e150f161a075c1ad38a3f6d67489ba7b9fdf
Oleg Chursin <olegchursin@gmail.com>
2022-10-31 01:31:56 -0400

    F - More styling tweaks

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,29 +1,34 @@
-import { FC } from 'react';
-import { useAtom } from 'jotai';
-import { uiAtom } from '../utils/store';
+import Link from 'next/link';
 import { clsx } from 'clsx';
+import { FC } from 'react';
+import { uiAtom } from '../utils/store';
+import { useAtom } from 'jotai';
 
 const Logo: FC = () => {
   const [{ sidebarOpen }] = useAtom(uiAtom);
 
   return (
-    <div
-      className={clsx('flex items-center', { 'justify-center': !sidebarOpen })}
-    >
-      <img
-        src="https://ik.imagekit.io/olegchursin/oc-profile_LypqvWxYJ.jpeg?tr=w-300,h-300"
-        className="w-10 rounded-full duration-300"
-      />
-      <h1
-        className={clsx(
-          'ml-4 origin-left font-montserrat text-xl font-medium text-transparent',
-          'bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text',
-          { hidden: !sidebarOpen }
-        )}
+    <Link href="/">
+      <div
+        className={clsx('flex cursor-pointer items-center', {
+          'justify-center': !sidebarOpen
+        })}
       >
-        Oleg&nbsp;Chursin
-      </h1>
-    </div>
+        <img
+          src="https://ik.imagekit.io/olegchursin/oc-profile_LypqvWxYJ.jpeg?tr=w-300,h-300"
+          className="w-10 rounded-full duration-300"
+        />
+        <h1
+          className={clsx(
+            'ml-4 origin-left font-montserrat text-xl font-medium text-transparent',
+            'bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text',
+            { hidden: !sidebarOpen }
+          )}
+        >
+          Oleg&nbsp;Chursin
+        </h1>
+      </div>
+    </Link>
   );
 };
 
