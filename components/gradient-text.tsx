446ff9233dae258229a2a30a067f16954360a669
Oleg Chursin <olegchursin@gmail.com>
2022-11-01 01:24:29 -0400

    F - Add GradientText component

---
+++ components/gradient-text.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import { FC } from 'react';
+
+interface GradientTextProps {
+  readonly text: string;
+  readonly hue: string;
+}
+
+const GradientText: FC<GradientTextProps> = ({ text, hue }) => {
+  return (
+    <span
+      className={clsx(
+        'dark:bg-gradient-to-r dark:from-sky-400 dark:to-blue-600 dark:bg-clip-text dark:text-transparent'
+      )}
+    >
+      {text}
+    </span>
+  );
+};
+
+export default GradientText;
