2e12be3358f4dd1c4a7add03141d07d3c7bc234e
Oleg Chursin <olegchursin@gmail.com>
2022-11-01 09:33:07 -0400

    F - Update writing view title

--- pages/writing.tsx
+++ pages/writing.tsx
@@ -1,11 +1,14 @@
 import Article from '../components/article';
+import GradientText from '../components/gradient-text';
 import { getPosts } from '../utils/posts';
 import type { NextPage } from 'next';
 
 const Writing: NextPage<any> = ({ posts }) => {
   return (
     <Article>
-      <h1>Latest Posts</h1>
+      <h1>
+        <GradientText text={'Latest Posts'} />
+      </h1>
 
       {posts?.map(post => console.log('post', post))}
     </Article>
