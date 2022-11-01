98bca31af980cffd8d72b7bc5aa3a9f091153ff3
Oleg Chursin <olegchursin@gmail.com>
2022-11-01 02:16:30 -0400

    B - Fix container layout for small screens

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import { FC, PropsWithChildren } from 'react';
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="container flex">
+    <div className="flex 2xl:container">
       <Sidebar />
       <main className="relative flex max-h-screen w-full flex-1 flex-col overflow-y-auto bg-gray-100 antialiased duration-300  dark:bg-gray-900 dark:text-white">
         <Navbar />
