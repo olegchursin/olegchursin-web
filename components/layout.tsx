25d18342e21b9649c967ae5c761ad7e868895f3b
Oleg Chursin <olegchursin@gmail.com>
2022-11-04 17:34:58 -0400

    F - Add padding to the overall layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,7 +8,7 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
       <Sidebar />
       <main className="relative flex max-h-screen w-full flex-1 flex-col overflow-y-auto bg-gray-100 antialiased duration-300  dark:bg-gray-900 dark:text-white">
         <Navbar />
-        <div className="container py-6 lg:max-w-3xl">{children}</div>
+        <div className="container p-6 lg:max-w-3xl">{children}</div>
       </main>
     </div>
   );
