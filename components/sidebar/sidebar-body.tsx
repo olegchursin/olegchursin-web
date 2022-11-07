15cefdf7feb88b43a52acc1a16bfff8dd7ceff1d
Oleg Chursin <olegchursin@gmail.com>
2022-11-07 02:43:04 -0500

    F - Create SidebarToggle component

--- components/sidebar/sidebar-body.tsx
+++ components/sidebar/sidebar-body.tsx
@@ -1,30 +1,12 @@
 import SidebarLinks from './sidebar-links';
-import { clsx } from 'clsx';
-import { FaChevronLeft } from 'react-icons/fa';
+import SidebarToggle from './sidebar-toggle';
 import { FC } from 'react';
-import { uiAtom } from '../../utils/store';
-import { useAtom } from 'jotai';
 
 const SidebarBody: FC = () => {
-  const [{ sidebarOpen }, setOpen] = useAtom(uiAtom);
-
-  const toggleSidebarOpen = () => {
-    setOpen({ sidebarOpen: !sidebarOpen });
-  };
-
   return (
     <div className="h-100 flex flex-col justify-between px-6 pt-0">
       <SidebarLinks />
-
-      <div className="p-4 duration-300 dark:bg-gray-800">
-        <FaChevronLeft
-          className={clsx(
-            'absolute right-7 bottom-4 h-6 w-6 cursor-pointer dark:text-gray-100',
-            { 'rotate-180': !sidebarOpen }
-          )}
-          onClick={toggleSidebarOpen}
-        />
-      </div>
+      <SidebarToggle />
     </div>
   );
 };
---
+++ components/sidebar/sidebar-toggle.tsx
@@ -0,0 +1,27 @@
+import { clsx } from 'clsx';
+import { FaChevronLeft } from 'react-icons/fa';
+import { FC } from 'react';
+import { uiAtom } from '../../utils/store';
+import { useAtom } from 'jotai';
+
+const SidebarToggle: FC = () => {
+  const [{ sidebarOpen }, setOpen] = useAtom(uiAtom);
+
+  const toggleSidebarOpen = () => {
+    setOpen({ sidebarOpen: !sidebarOpen });
+  };
+
+  return (
+    <div className="p-4 duration-300 dark:bg-gray-800">
+      <FaChevronLeft
+        className={clsx(
+          'absolute right-7 bottom-4 h-6 w-6 cursor-pointer dark:text-gray-100',
+          { 'rotate-180': !sidebarOpen }
+        )}
+        onClick={toggleSidebarOpen}
+      />
+    </div>
+  );
+};
+
+export default SidebarToggle;
