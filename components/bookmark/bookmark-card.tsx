036d53e82a86be66b51ead8fdc54f37ba2614b30
Oleg Chursin <olegchursin@gmail.com>
2022-11-07 02:39:20 -0500

    F - Playing with bookmark styles

--- components/bookmark/bookmark-card.tsx
+++ components/bookmark/bookmark-card.tsx
@@ -1,4 +1,6 @@
+import clsx from 'clsx';
 import { FC } from 'react';
+import { FaExternalLinkSquareAlt } from 'react-icons/fa';
 
 interface BookmarkCardProps {
   readonly bookmark: any;
@@ -7,10 +9,20 @@ interface BookmarkCardProps {
 
 const BookmarkCard: FC<BookmarkCardProps> = ({ bookmark, index }) => {
   return (
-    <div className="rounded-lg border-[0.5px] p-4">
-      <h4>{bookmark.given_title ?? `Bookmark ${index}`}</h4>
-      <p>{bookmark.excerpt ?? ''}</p>
-      <a href={bookmark.giveen_url ?? bookmark.resolved_url}>Link</a>
+    <div
+      className={clsx(
+        'rounded-lg border-[0.5px] dark:border-gray-800 dark:hover:border-blue-800',
+        'bg-white p-4 dark:bg-gray-800'
+      )}
+    >
+      <a href={bookmark.giveen_url ?? bookmark.resolved_url}>
+        <div className="flex items-center justify-between">
+          <div className="m-0">
+            {bookmark.given_title ?? `Bookmark ${index}`}
+          </div>
+          <FaExternalLinkSquareAlt />
+        </div>
+      </a>
     </div>
   );
 };
