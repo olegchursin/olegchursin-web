e3e42bc17be37bde82f5909359243822a70c4d93
Oleg Chursin <olegchursin@gmail.com>
2022-11-09 15:55:03 -0500

    F - Fix bookmarks card styling

--- components/bookmark/bookmark-card.tsx
+++ components/bookmark/bookmark-card.tsx
@@ -9,21 +9,39 @@ interface BookmarkCardProps {
 
 const BookmarkCard: FC<BookmarkCardProps> = ({ bookmark, index }) => {
   return (
-    <div
-      className={clsx(
-        'rounded-lg border-[0.5px] dark:border-gray-800 dark:hover:border-blue-800',
-        'bg-white p-4 dark:bg-gray-800'
-      )}
+    <a
+      href={bookmark.giveen_url ?? bookmark.resolved_url}
+      target="_blank"
+      rel="noopener"
     >
-      <a href={bookmark.giveen_url ?? bookmark.resolved_url}>
-        <div className="flex items-center justify-between">
-          <div className="m-0">
-            {bookmark.given_title ?? `Bookmark ${index}`}
+      <div
+        className={clsx(
+          'rounded-lg border-[0.5px] dark:border-gray-800 dark:hover:border-blue-800',
+          'bg-white p-4 dark:bg-gray-800',
+          'flex justify-between gap-8'
+        )}
+      >
+        <div className="flex flex-col gap-4 md:flex-row">
+          <img
+            className="h-10 w-10 rounded-lg"
+            src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+            alt=""
+          />
+          <div className="font-medium dark:text-white">
+            <div className="md:-mt-2">
+              {bookmark.given_title ?? `Bookmark ${index}`}
+            </div>
+            <div className="text-sm text-gray-500 dark:text-gray-400">
+              {bookmark.excerpt ?? null}
+            </div>
           </div>
+        </div>
+
+        <div className="flex-shrink-0 flex-grow-0 basis-4">
           <FaExternalLinkSquareAlt />
         </div>
-      </a>
-    </div>
+      </div>
+    </a>
   );
 };
 
--- components/bookmark/bookmarks-grid.tsx
+++ components/bookmark/bookmarks-grid.tsx
@@ -7,7 +7,7 @@ interface BookmarksGridProps {
 
 const BookmarksGrid: FC<BookmarksGridProps> = ({ bookmarks }) => {
   return (
-    <div className="grid grid-flow-row gap-4">
+    <div className="not-prose grid grid-flow-row gap-4">
       {Object.values(bookmarks).map((bookmark: any, idx: number) => {
         return (
           <BookmarkCard
