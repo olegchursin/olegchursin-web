3c59d45ed59e5f905ba00f9f01b2354b1acf3a6e
Oleg Chursin <olegchursin@gmail.com>
2022-11-09 17:17:20 -0500

    R - More bookmark-card styles

--- components/bookmark/bookmark-card.tsx
+++ components/bookmark/bookmark-card.tsx
@@ -8,6 +8,9 @@ interface BookmarkCardProps {
 }
 
 const BookmarkCard: FC<BookmarkCardProps> = ({ bookmark, index }) => {
+  const titlePlaceholder = `Bookmark ${parseInt(index as string, 10) + 1}`;
+  const title = bookmark.given_title ? bookmark.given_title : titlePlaceholder;
+
   return (
     <a
       href={bookmark.giveen_url ?? bookmark.resolved_url}
@@ -17,20 +20,17 @@ const BookmarkCard: FC<BookmarkCardProps> = ({ bookmark, index }) => {
       <div
         className={clsx(
           'rounded-lg border-[0.5px] dark:border-gray-800 dark:hover:border-blue-800',
-          'bg-white p-4 dark:bg-gray-800',
+          'bg-white p-4 duration-300 dark:bg-gray-800',
           'flex justify-between gap-8'
         )}
       >
         <div className="flex flex-col gap-4 md:flex-row">
-          <img
-            className="h-10 w-10 rounded-lg"
-            src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-            alt=""
-          />
+          <div className="flex h-10 w-10 flex-shrink-0 flex-grow-0 basis-10 items-center justify-center rounded-lg bg-gray-300 dark:bg-gray-600">
+            {title[0]}
+          </div>
+
           <div className="font-medium dark:text-white">
-            <div className="md:-mt-2">
-              {bookmark.given_title ?? `Bookmark ${index}`}
-            </div>
+            <div className="md:-mt-2">{title}</div>
             <div className="text-sm text-gray-500 dark:text-gray-400">
               {bookmark.excerpt ?? null}
             </div>
