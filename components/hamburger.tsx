303cc98d5ca1d39bbad894d3f8366e5943afb775
Oleg Chursin <olegchursin@gmail.com>
2022-11-09 15:55:27 -0500

    R - Fix attribute casing

--- components/hamburger.tsx
+++ components/hamburger.tsx
@@ -17,9 +17,9 @@ const Hamburger: FC = () => {
       stroke="currentColor"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="M4 6h16M4 12h8m-8 6h16"
       />
     </svg>
